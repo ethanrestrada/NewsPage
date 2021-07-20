f90e2cb1e51e612773b23e988a08a159cbc54f7d
Ethan Ruiz <etanr25@gmail.com>
2021-07-19 19:22:31 -0600

    v.06-white

--- index.js
+++ index.js
@@ -4,4 +4,18 @@ const indicador = document.querySelector(".indicador");
     btnSwitch.onclick = function(){
         document.body.classList.toggle("white");
         indicador.classList.toggle("active");
+      
+      if(document.body.classList.contains("white")){
+        localStorage.setItem("white-mode", "true");
+      } else {
+        localStorage.setItem("white-mode", "false");
+      }
     }
+
+if(localStorage.getItem("white-mode") === "true" ){
+  document.body.classList.add("white");
+  indicador.classList.add("active");
+} else {
+  document.body.classList.remove("white");
+  indicador.classList.remove("active");
+}
